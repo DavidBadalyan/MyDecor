b840843653ecb184cbcb909d1cfd3a39e51d0008
David Badalyan <143111402+DavidBadalyan@users.noreply.github.com>
2024-03-05 20:44:25 +0400

    Add files via upload

---
+++ MyDecor/src/main/java/org/example/mydecor/MyDecorApplication.java
@@ -0,0 +1,13 @@
+package org.example.mydecor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDecorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDecorApplication.class, args);
+    }
+
+}
---
+++ MyDecor/src/test/java/org/example/mydecor/MyDecorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mydecor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDecorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
